317458b1fa37d4e6621ffc2a52e52e98d5fd6f77
GabrielaDiazB <gabriela.dbravo@gmail.com>
2018-12-17 13:29:02 -0500

    se añadió la funcionabilidad del botón de búsqueda, filtrado y ordenado. Se añadió un switch en el html. Primera parte de separación de funciones en el main y en el data

--- src/data.js
+++ src/data.js
@@ -1,8 +1,65 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
+const filterDataFunction = (data, dataFilter, condition) => {
+  let dataCopy = [];
+  let arrayFilter = [];
+  let newArrayFilter = [];
+
+  for (let i = 0; i < data.length; i++)
+    dataCopy.push(Object.assign({}, data[i]));
+
+  if (dataFilter.length === 0)
+    return dataCopy;
+
+  switch (condition) {
+   
+    case 0:
+      for (let i = 0; i < dataCopy.length; i++) {
+        arrayFilter.push(dataCopy[i].name.toLowerCase());
+        if (arrayFilter[i].indexOf(dataFilter.toLowerCase()) !== -1)
+          newArrayFilter.push(dataCopy[i]);
+      }
+      break;
+    default: alert('No existe opcion');
+  }
+  return newArrayFilter;
 };
 
-window.example = example;
+//Sort
+const sortDataFunction = (data, sortBy, sortOrder) => {
+
+  let newArrayFilter = [];
+ 
+  for (let i = 0; i < data.length; i++)
+    newArrayFilter.push(Object.assign({}, data[i]));
+ 
+  if (sortBy === 0) {
+    //Ordenas por ID
+    newArrayFilter.sort(
+      function (a, b) {
+        if (sortOrder === 0) return a.info.difficulty - b.info.difficulty;
+        else return b.info.difficulty - a.info.difficulty;
+      }
+    );
+  }
+  else {
+    //Ordenar por A-Z
+    newArrayFilter.sort(
+      function (a, b) {
+        if (sortOrder === 0) {
+          if (a.name > b.name) return 1;
+          else return -1;
+        }
+        else {
+          if (a.name < b.name) return 1;
+          else return -1;
+        }
+      }
+    );
+  }
+  return newArrayFilter;
+ };
+
+ window.data = {
+   filterData: filterDataFunction,
+   sortData: sortDataFunction,
+ }
--- src/main.js
+++ src/main.js
@@ -1,61 +1,114 @@
 const arrKeys = Object.values(LOL.data);
+const sortBy = document.getElementById("lol-order");
+const filtersByCategories = document.getElementById("lol-filter");
+const inputSearchText = document.getElementById('search-champion');
+const btnSearch = document.getElementById('search-btn');
+
+
 
 //Recorre la data y plasma en el html img, name y title
 const containerList=document.getElementById("container-list");
 const createTemplateCard = (list) => {
   let templateListCard = '';
-  list.forEach((arrKeys)=>{
+  list.forEach((arrKeys,index)=>{
     const card=`
     <div class ="card-link">
-    <button class="blog-card">
+    <button class="blog-card" id="${ index }">
     <img class="post-image" src="${ arrKeys.img }" />
     <div class="article-details">
     <h1 class="post-name">${ arrKeys.name}</h1>
     <h3 class="post-title"> ${ arrKeys.title}</h3>
+    <h3 class="post-title"> ${ arrKeys.info.difficulty}</h3>
+
     </div>
     </button>
     </div>`;
     templateListCard += card;
+
   })
-  containerList.innerHTML=templateListCard
+  containerList.innerHTML=templateListCard;
+  
 };
 createTemplateCard(arrKeys);
 
 
 
-//Recorrer y devuelve el URL del icono, el nombre y su título
-for (let i = 0; i < arrKeys.length; i++){
-    //const arr = [];
-    const champions = arrKeys[i];
-    const imgChampions = (`${champions.img} ${champions.name} ${champions.title} </br>`);
-    //arr[i] = imgChampions;
-    // document.write(imgChampions);
-    console.log(imgChampions);
-};
+const abc = document.getElementById('2');
+abc.addEventListener('click', function(){console.log('hola')});
+
+
+// //Recorrer y devuelve el URL del icono, el nombre y su título
+// for (let i = 0; i < arrKeys.length; i++){
+//     //const arr = [];
+//     const champions = arrKeys[i];
+//     const imgChampions = (`${champions.img} ${champions.name} ${champions.title} </br>`);
+//     //arr[i] = imgChampions;
+//     // document.write(imgChampions);
+//     
+// };
 
 
 //filtrado
 
-const filterFighter = arrKeys.filter(function (data) {
+const assassin = document.getElementById('assassin');
+assassin.addEventListener('click', functionFilterAssassin);
+function functionFilterAssassin(){
+  const filterAssassin = arrKeys.filter(function(data){
+    return data.tags.includes('Assassin')===true;
+  });
+  console.log(filterAssassin);
+};
+
+
+const fighter = document.getElementById('fighter');
+fighter.addEventListener('change', functionFilterFighter);
+function functionFilterFighter(){
+  const filterFighter = arrKeys.filter(function (data) {
   return data.tags.includes('Fighter')===true;
-});
+  });
+  console.log(filterFighter);
+};
 
-const filterAssassin = arrKeys.filter(function(data){
-  return data.tags.includes('Assassin')===true;
-});
 
-const filterMage = arrKeys.filter(function(data){
+const mage = document.getElementById('mage');
+mage.addEventListener('click', functionFilterMage);
+function functionFilterMage(){
+  const filterMage = arrKeys.filter(function(data){
   return data.tags.includes('Mage')===true;
-});
-const filterMarksman = arrKeys.filter(function(data){
-  return data.tags.includes('Marksman')===true;
-});
-const filterSupport = arrKeys.filter(function(data){
+  });
+  console.log(filterMage);
+};
+
+
+const marksman = document.getElementById('marksman');
+marksman.addEventListener('click', functionFilterMarksman);
+function functionFilterMarksman(){
+  const filterMarksman = arrKeys.filter(function(data){
+    return data.tags.includes('Marksman')===true;
+  });
+  console.log(filterMarksman);
+};
+
+
+const support = document.getElementById('support');
+support.addEventListener('click',functionFilterSupport);
+function functionFilterSupport(){
+  const filterSupport = arrKeys.filter(function(data){
   return data.tags.includes('Support')===true;
-});
-const filterTank = arrKeys.filter(function(data){
+  });
+  console.log(filterSupport);
+};
+
+
+const tank = document.getElementById('tank');
+tank.addEventListener('click',functionFilterTank);
+function functionFilterTank(){
+  const filterTank = arrKeys.filter(function(data){
   return data.tags.includes('Tank')===true;
-});
+  });
+  console.log(filterTank);
+};
+
 
 
 
@@ -63,3 +116,20 @@ const filterTank = arrKeys.filter(function(data){
 
 //Buscador 
 
+const functionListenFilterOrder = () => {
+  const listenSortBy = sortBy.options[sortBy.selectedIndex].value;
+  const listenFiltersByCategories = filtersByCategories.options[filtersByCategories.selectedIndex].value;
+  const arrayInputFilter = data.filterData(arrKeys, inputSearchText.value, parseInt(listenFiltersByCategories[0]));
+  
+  createTemplateCard(data.sortData(arrayInputFilter, parseInt(listenSortBy[0]), parseInt(listenSortBy[1])));
+  return 1;
+}
+
+const functionMain = () => {
+  functionListenFilterOrder();
+  filtersByCategories.addEventListener("change", functionListenFilterOrder);
+  sortBy.addEventListener("change", functionListenFilterOrder);
+  btnSearch.addEventListener('click', functionListenFilterOrder);
+}
+
+functionMain();
