83515144b457be8089ef08aca51133cfbb9bb0d0
GabrielaDiazB <gabriela.dbravo@gmail.com>
2018-12-19 18:11:18 -0500

    cambios por eslint y primera modificación para el modal

--- src/data.js
+++ src/data.js
@@ -10,55 +10,51 @@ const filterDataFunction = (data, dataFilter, condition) => {
   if (dataFilter.length === 0)
     return dataCopy;
 
-  switch (condition) {
-   
-    case 0:
-      for (let i = 0; i < dataCopy.length; i++) {
-        arrayFilter.push(dataCopy[i].name.toLowerCase());
-        if (arrayFilter[i].indexOf(dataFilter.toLowerCase()) !== -1)
-          newArrayFilter.push(dataCopy[i]);
-      }
-      break;
-    default: alert('No existe opcion');
+  switch (condition) {   
+  case 0:
+    for (let i = 0; i < dataCopy.length; i++) {
+      arrayFilter.push(dataCopy[i].name.toLowerCase());
+      if (arrayFilter[i].indexOf(dataFilter.toLowerCase()) !== -1)
+        newArrayFilter.push(dataCopy[i]);
+    }
+    break;
+  default: alert('No existe opcion');
   }
   return newArrayFilter;
 };
 
-//Ordenar
+// Ordenar
 const sortDataFunction = (data, sortBy, sortOrder) => {
-
   let newArrayFilter = [];
  
   for (let i = 0; i < data.length; i++)
     newArrayFilter.push(Object.assign({}, data[i]));
  
   if (sortBy === 0) {
-    //Ordenar por Alfabéticamente
+    // Ordenar por Alfabéticamente
     newArrayFilter.sort(
-      function (a, b) {
+      function(a, b) {
         if (sortOrder === 0) {
           if (a.name > b.name) return 1;
-        else return -1;
-        }
-         else {
+          else return -1;
+        } else {
           if (a.name < b.name) return 1;
           else return -1;
         }
       }
     );
-  }
-  else{
-  //Ordenar por Dificultad
-  newArrayFilter.sort(
-    function (a, b) {
-      if (sortOrder === 0) return a.info.difficulty - b.info.difficulty;
-      else return b.info.difficulty - a.info.difficulty;
-    });
+  } else {
+  // Ordenar por Dificultad
+    newArrayFilter.sort(
+      function(a, b) {
+        if (sortOrder === 0) return a.info.difficulty - b.info.difficulty;
+        else return b.info.difficulty - a.info.difficulty;
+      });
   }
   return newArrayFilter;
- };
+};
 
- window.data = {
-   filterData: filterDataFunction,
-   sortData: sortDataFunction,
- }
+window.data = {
+  filterData: filterDataFunction,
+  sortData: sortDataFunction,
+};
--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ const createTemplateCard = (list) => {
   list.forEach((arrKeys, index) => {
     const card = `
     <div class ="card-link">
-    <button class="blog-card" id="${ index }">
+    <a class="blog-card" id="${ index }" href="#open-modal">
     <img class="post-image" src="${ arrKeys.img }" />
     <div class="article-details">
     <h1 class="post-name">${ arrKeys.name}</h1>
@@ -20,18 +20,18 @@ const createTemplateCard = (list) => {
     <h3 class="post-title"> <img class="difficulty-img" src="img/difficulty.jpg"/> ${ arrKeys.info.difficulty}</h3>
 
     </div>
-    </button>
+    </a>
     </div>`;
     templateListCard += card;
-  })
+  });
   containerList.innerHTML = templateListCard;
 };
 createTemplateCard(arrKeys);
 
-
-
 const abc = document.getElementById('0');
-abc.addEventListener('click', function(){console.log('hola')});
+abc.addEventListener('click', function() {
+  console.log('hola');
+});
 
 // filtrado
 
@@ -39,7 +39,7 @@ const assassin = document.getElementById('assassin');
 assassin.addEventListener('click', functionFilterAssassin);
 function functionFilterAssassin() {
   const filterAssassin = arrKeys.filter(function(data) {
-    return data.tags.includes('Assassin')===true;
+    return data.tags.includes('Assassin') === true;
   });
   console.log(filterAssassin);
 }
@@ -47,59 +47,55 @@ function functionFilterAssassin() {
 
 const fighter = document.getElementById('fighter');
 fighter.addEventListener('change', functionFilterFighter);
-function functionFilterFighter(){
-  const filterFighter = arrKeys.filter(function (data) {
-  return data.tags.includes('Fighter')===true;
+function functionFilterFighter() {
+  const filterFighter = arrKeys.filter(function(data) {
+    return data.tags.includes('Fighter') === true;
   });
   console.log(filterFighter);
-};
+}
 
 
 const mage = document.getElementById('mage');
 mage.addEventListener('click', functionFilterMage);
-function functionFilterMage(){
-  const filterMage = arrKeys.filter(function(data){
-  return data.tags.includes('Mage')===true;
+function functionFilterMage() {
+  const filterMage = arrKeys.filter(function(data) {
+    return data.tags.includes('Mage') === true;
   });
   console.log(filterMage);
-};
+}
 
 
 const marksman = document.getElementById('marksman');
 marksman.addEventListener('click', functionFilterMarksman);
-function functionFilterMarksman(){
-  const filterMarksman = arrKeys.filter(function(data){
-    return data.tags.includes('Marksman')===true;
+function functionFilterMarksman() {
+  const filterMarksman = arrKeys.filter(function(data) {
+    return data.tags.includes('Marksman') === true;
   });
   console.log(filterMarksman);
-};
+}
 
 
 const support = document.getElementById('support');
-support.addEventListener('click',functionFilterSupport);
-function functionFilterSupport(){
-  const filterSupport = arrKeys.filter(function(data){
-  return data.tags.includes('Support')===true;
+support.addEventListener('click', functionFilterSupport);
+function functionFilterSupport() {
+  const filterSupport = arrKeys.filter(function(data) {
+    return data.tags.includes('Support') === true;
   });
   console.log(filterSupport);
-};
+}
 
 
 const tank = document.getElementById('tank');
-tank.addEventListener('click',functionFilterTank);
-function functionFilterTank(){
-  const filterTank = arrKeys.filter(function(data){
-  return data.tags.includes('Tank')===true;
+tank.addEventListener('click', functionFilterTank);
+function functionFilterTank() {
+  const filterTank = arrKeys.filter(function(data) {
+    return data.tags.includes('Tank') === true;
   });
   console.log(filterTank);
-};
+}
 
 
-
-
-
-
-//Buscador 
+// Buscador 
 
 const functionListenFilterOrder = () => {
   const listenSortBy = sortBy.options[sortBy.selectedIndex].value;
@@ -108,13 +104,13 @@ const functionListenFilterOrder = () => {
   
   createTemplateCard(data.sortData(arrayInputFilter, parseInt(listenSortBy[0]), parseInt(listenSortBy[1])));
   return 1;
-}
+};
 
 const functionMain = () => {
   functionListenFilterOrder();
-  filtersByCategories.addEventListener("change", functionListenFilterOrder);
-  sortBy.addEventListener("change", functionListenFilterOrder);
+  filtersByCategories.addEventListener('change', functionListenFilterOrder);
+  sortBy.addEventListener('change', functionListenFilterOrder);
   btnSearch.addEventListener('click', functionListenFilterOrder);
-}
+};
 
 functionMain();
